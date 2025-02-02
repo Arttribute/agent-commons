9b335d71dc4b0e34abf186cc8a6b50161d37e253
bashybaranaba <bashybaranaba@gmail.com>
2025-02-02 14:50:08 +0000

    feat: set up auth with privy

--- apps/commons-app/app/layout.tsx
+++ apps/commons-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/Providers"; // The file with your <PrivyProvider> from earlier
+import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          <AuthProvider>{children}</AuthProvider>
+        </Providers>
       </body>
     </html>
   );
