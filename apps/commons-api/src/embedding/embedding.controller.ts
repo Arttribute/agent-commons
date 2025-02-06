402b0bce90b6fdfc5a2a706ffdc0964bff790a27
khalifa47 <khalifafumo5@gmail.com>
2025-02-06 23:39:28 +0100

    fix: disable ESlint warnings for embeddings service
    The warning were just noisy. Took precautions though

--- apps/commons-api/src/embedding/embedding.controller.ts
+++ apps/commons-api/src/embedding/embedding.controller.ts
@@ -1,35 +1,19 @@
-import {
-  Controller,
-  Post,
-  Body,
-  // UseInterceptors,
-  // UploadedFile,
-  HttpCode,
-} from '@nestjs/common';
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { EmbeddingService } from './embedding.service';
 import { EmbeddingDto } from './dto/embedding.dto';
-// import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('embedding')
 export class EmbeddingController {
   constructor(private readonly embeddingService: EmbeddingService) {}
 
   @Post()
-  // @UseInterceptors(FileInterceptor('file'))
-  create(
-    @Body() createEmbeddingDto: EmbeddingDto,
-    // @UploadedFile() file?: Express.Multer.File,
-  ) {
+  create(@Body() createEmbeddingDto: EmbeddingDto) {
     return this.embeddingService.create(createEmbeddingDto);
   }
 
   @Post('find')
   @HttpCode(200)
-  // @UseInterceptors(FileInterceptor('file'))
-  find(
-    // @UploadedFile() file?: Express.Multer.File,
-    @Body() searchEmbeddingDto: EmbeddingDto,
-  ) {
+  find(@Body() searchEmbeddingDto: EmbeddingDto) {
     return this.embeddingService.find(searchEmbeddingDto);
   }
 }
