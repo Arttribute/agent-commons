7b9b7b525905c7d63d585586d8025cd82905afa1
khalifa47 <khalifafumo5@gmail.com>
2025-02-06 22:17:33 +0100

    feat: add support for audio
    Only support wav files at the moment

--- apps/commons-api/src/embedding/embedding.service.ts
+++ apps/commons-api/src/embedding/embedding.service.ts
@@ -39,8 +39,16 @@ export class EmbeddingService {
   }
 
   private async loadAudioPipeline() {
-    const { pipeline } = await import('@xenova/transformers');
-    return pipeline('feature-extraction', 'Xenova/vq-wav2vec-base');
+    const { AutoProcessor, ClapAudioModelWithProjection } = await import(
+      '@xenova/transformers'
+    );
+    const processor = await AutoProcessor.from_pretrained(
+      'Xenova/larger_clap_general',
+    );
+    const model = await ClapAudioModelWithProjection.from_pretrained(
+      'Xenova/larger_clap_general',
+    );
+    return { processor, model };
   }
 
   private async embed(content: string, type: EmbeddingType): Promise<number[]> {
@@ -63,12 +71,34 @@ export class EmbeddingService {
         });
         break;
       case EmbeddingType.audio: {
-        generateEmbedding = await this.loadAudioPipeline();
-        output = await generateEmbedding(content, {
-          pooling: 'mean',
-          normalize: true,
-        });
-        // TODO: Add support for audio files
+        const { processor, model } = await this.loadAudioPipeline();
+        const buffer = Buffer.from(
+          await fetch(content).then((x) => x.arrayBuffer()),
+        );
+        const wav = new WaveFile(buffer);
+        wav.toBitDepth('32f');
+        wav.toSampleRate(48000);
+
+        let audioData = wav.getSamples();
+
+        if (Array.isArray(audioData)) {
+          if (audioData.length > 1) {
+            const SCALING_FACTOR = Math.sqrt(2);
+
+            // Merge channels (into first channel to save memory)
+            for (let i = 0; i < audioData[0].length; ++i) {
+              audioData[0][i] =
+                (SCALING_FACTOR * (audioData[0][i] + audioData[1][i])) / 2;
+            }
+          }
+
+          // Select first channel
+          audioData = audioData[0];
+          const audioInputs = await processor(audioData);
+          const { audio_embeds } = await model(audioInputs);
+          output = audio_embeds;
+        }
+        break;
         break;
       }
       default:
