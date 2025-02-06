e9c1a058103b30ee0c0a3229222a80b2460560e2
bashybaranaba <bashybaranaba@gmail.com>
2025-02-06 15:05:04 +0000

    fix: return navbar

--- apps/commons-app/app/page.tsx
+++ apps/commons-app/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { useAuth } from "../context/AuthContext"; // the file we created
+import AppBar from "@/components/layout/AppBar";
 
 export default function HomePage() {
   const { authState, login, logout } = useAuth();
@@ -9,36 +10,39 @@ export default function HomePage() {
   const isAuthenticated = !!idToken;
 
   return (
-    <main style={{ padding: 40 }}>
-      <h1>Welcome to My Next.js + Privy App</h1>
+    <div>
+      <AppBar />
+      <div className="min-h-screen  mt-16">
+        <h1>Welcome to My Next.js + Privy App</h1>
 
-      {isAuthenticated ? (
-        <>
-          <div style={{ margin: "20px 0" }}>
-            <img
-              src={profileImage ?? ""}
-              alt="profile"
-              style={{ width: 80, height: 80, borderRadius: "50%" }}
-            />
-            <p>
-              <strong>Username:</strong> {username}
-            </p>
-            <p>
-              <strong>Wallet:</strong> {walletAddress}
-            </p>
-            <p>
-              <strong>ID Token:</strong> {idToken?.slice(0, 20)}...{" "}
-            </p>
-          </div>
+        {isAuthenticated ? (
+          <>
+            <div style={{ margin: "20px 0" }}>
+              <img
+                src={profileImage ?? ""}
+                alt="profile"
+                style={{ width: 80, height: 80, borderRadius: "50%" }}
+              />
+              <p>
+                <strong>Username:</strong> {username}
+              </p>
+              <p>
+                <strong>Wallet:</strong> {walletAddress}
+              </p>
+              <p>
+                <strong>ID Token:</strong> {idToken?.slice(0, 20)}...{" "}
+              </p>
+            </div>
 
-          <button onClick={logout}>Logout</button>
-        </>
-      ) : (
-        <>
-          <p>You are not logged in</p>
-          <button onClick={login}>Login with Privy</button>
-        </>
-      )}
-    </main>
+            <button onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <p>You are not logged in</p>
+            <button onClick={login}>Login with Privy</button>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
